22aefca7aae0a93b7b9ba04c1a6c1c99c628f5c2
uliana <uliana@accbox.info>
2019-12-27 17:47:17 +0200

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {Image, ImageBackground, Platform, StyleSheet, TouchableOpacity, View, ViewPropTypes} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import Video from 'react-native-video'
+
+export default class Video extends Component {
+    render() {
+        return (
+            <View>
+                
+            </View>
+        );
+    }
+}
